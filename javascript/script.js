a57338e6e09f839f158753149daf3bea5e0bd11f
ZappyMan <chimiene@oregonstate.edu>
2019-11-28 14:41:49 -0800

    Working dynmaic window sizing
    Side bar now sizes proportionally to window size.

--- javascript/script.js
+++ javascript/script.js
@@ -15,3 +15,23 @@ let map = tomtom.L.map('map', {
 map.addEventListener('click', (event) => {
     tomtom.L.marker(event.latlng).addTo(map);
 });
+
+//^^ Map API Junk
+
+var screenWidth = window.screen.width * window.devicePixelRatio;
+var sideBar = document.getElementById('side-elements');
+var clientWidth = window.innerWidth;
+var sideBarWidth = screenWidth/5;
+
+//Resizing map/sidebar for different window sizes
+window.addEventListener('resize', function(event){
+  clientWidth = window.innerWidth;
+  document.getElementById('map').style.width = (((clientWidth-sideBarWidth)/clientWidth)*100)+"vw";
+  sideBar.style.width = 100-(((clientWidth-sideBarWidth)/clientWidth)*100)+"vw"
+  //if(window.screen.width < )
+  //sideBar.style.width = width+"px";
+  //document.getElementById('map').style.width = (document.clientWidth-width)+"px";
+  console.log("window.outerWidth: ",window.innerWidth);
+  console.log("window.screen.width * window.devicePixelRatio",window.screen.width * window.devicePixelRatio);
+
+});
