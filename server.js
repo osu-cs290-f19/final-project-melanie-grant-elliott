1c3e861d8a738d0ca2b973e47bd7caca3a9e9a27
Melanie <mirrorkeydev@gmail.com>
2019-12-08 21:05:48 -0800

    added cat marker, preload markers from db, + more

--- server.js
+++ server.js
@@ -26,11 +26,12 @@ MongoClient.connect(url, (err, database) => {
 
     // Tell the server where the css and javascript are located
     app.use('/css', express.static(__dirname + '/css'));
+    app.use('/images', express.static(__dirname + '/images'));
     app.use('/javascript', express.static(__dirname + '/javascript'));
     app.use('/sdk', express.static(__dirname + '/sdk'));
     app.use(express.json());
 
-    // GET /
+    // GET / - gets index.html
     app.get('/', async function(req, res){
 
         // This gets cats that were spotted in the last 24 hours
@@ -44,22 +45,35 @@ MongoClient.connect(url, (err, database) => {
         // so that we can pass the backend cats to the front end
     });
 
-    // POST /cat-spotting
+    // GET /cat-locations - gets all cats from the database as a list
+    // This is a helper function to script.js that allows the map to load all cat spottings as markers
+    app.get('/cat-locations', async function(req, res){
+
+        // This gets cats that were spotted in the last 24 hours
+        // this array contains cats in the form of [ {_id: 1, name: "cat name", lat: 123, long: 145, createdAt: date}, ... ]
+        let cats = await db.collection('cat-spottings')
+            .find({"createdAt":{$gt:new Date(Date.now() - 24*60*60 * 1000)}}).toArray();
+        res.status(200).send(cats);
+    });
+
+    // POST /cat-spotting - uploads a new cat to the database
     // example POST:
-    // { "name" : "Cat McCatFace", "lat" : 44.5242, "long" : -123.2792 }
+    // { "lat" : 44.5242, "long" : -123.2792, "color" : "white", ...etc }
     app.post('/cat-spotting', async function(req, res){
 
         // Parse the request, and use the body's values to create a new entry in the database
         await db.collection('cat-spottings').insertOne(
             {
-                "name" : req.body.name,
                 "lat" : req.body.lat,
                 "long" : req.body.long,
+                "color" : req.body.color,
+                "energy" : req.body.energy,
+                "sociability" : req.body.sociability,
                 "createdAt" : new Date() //the date the POST is made is added automatically
             }
         );
 
-        console.log("Saved cat: \"" + req.body.name +  "\" at lat: " + req.body.lat + " and long: " + req.body.long + " to the db!");
+        console.log("Saved " + req.body.color + " + cat at lat: " + req.body.lat + " and long: " + req.body.long + " to the db!");
 
         // When a POST request is made, the main page (index.html) is reloaded so that the newest cat will
         // show up in both the map and the timeline.
