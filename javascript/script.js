c6ad722f53419588a952a2f00e3116c0bcf03c3b
ZappyMan <chimiene@oregonstate.edu>
2019-12-10 15:20:36 -0800

    Added catspot logo
    Added catspot logo into index.html. Conflicting javascript, waiting for melanie to push to master.

--- javascript/script.js
+++ javascript/script.js
@@ -13,8 +13,46 @@ let map = tomtom.L.map('map', {
 });
 
 map.addEventListener('click', (event) => {
+<<<<<<< Updated upstream
     tomtom.L.marker(event.latlng).addTo(map);
 });
+=======
+    // Add new marker to map
+    let addNewMarker = tomtom.L.marker(event.latlng, {icon: catIcon});
+    addNewMarker.addTo(map);
+    addNewMarker.addEventListener('click', handleMarkerClick);
+    markersList.push(addNewMarker);
+
+    // TODO: upon clicking on the map and adding a marker, the modal should appear
+});
+
+// Creates a new cat in the database. Example call:
+// createNewCat(44.5125,-123.2691,"white","high","shy");
+async function createNewCat(rlat, rlong, rcolor, renergy, rsociability){
+
+    let postData = {
+        lat : rlat,
+        long : rlong,
+        color : rcolor,
+        energy : renergy,
+        sociability : rsociability
+    };
+
+    // Default options are marked with *
+    const response = await fetch('./cat-spotting', {
+        method: 'POST',
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrer: 'no-referrer', // no-referrer, *client
+        body: JSON.stringify(postData) // body data type must match "Content-Type" header
+      });
+>>>>>>> Stashed changes
 
 //^^ Map API Junk
 
@@ -30,3 +68,14 @@ function insertNewCat(image, title, color, kindness, address){
   var photoCardContainer = document.querySelector('#cat-tivity');
   photoCardContainer.insertAdjacentHTML('beforeend', newPhotoCardHTML);
 }
+<<<<<<< Updated upstream
+=======
+
+// TODO: add event listener on modal "accept" button that takes the fields,
+// wraps them in a POST request to /cat-spotting, and then clears them, closing the modal.
+// use the createNewCat(...) function to send a POST request
+
+// TODO: add event listener on modal "cancel" button that clears the fields,
+// closes the modal, and removes the last placed marker (as we are not going through with the new cat)
+// use map.removeLayer(markersList[markersList.length-1]) to remove the last marker
+>>>>>>> Stashed changes
